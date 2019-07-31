13624f94849316988a25c5ecd1f5921aba017295
jl513 <lin727397>
2019-07-31 14:29:56 +1200

    fix location update

--- app/src/main/java/com/example/a318_a1/MapsActivity.java
+++ app/src/main/java/com/example/a318_a1/MapsActivity.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.gesture.Gesture;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,7 +38,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private LocationManager locationManager;
@@ -196,6 +197,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void getLocation(){
         if(checkPermission()){
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
             location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             if(location != null){
                 lat = location.getLatitude();
@@ -203,6 +205,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("location", lat + ", " + lon);
             }else{
                 location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,this);
                 if(location != null){
                     lat = location.getLatitude();
                     lon = location.getLongitude();
@@ -320,4 +323,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
